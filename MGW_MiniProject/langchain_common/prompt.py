b015f7b486e63375b625f19d860e140af65b0f81
Jamesmoon00 <gwm999955@gmail.com>
2024-11-06 14:10:13 +0900

    2nd commit

--- MGW_MiniProject/langchain_common/prompt.py
+++ MGW_MiniProject/langchain_common/prompt.py
@@ -8,14 +8,19 @@ def create_message(role: CHATBOT_ROLE, prompt: str):
     return {CHATBOT_MESSAGE.role.name: role.name, CHATBOT_MESSAGE.content.name: prompt}
 
 
-def create_prompt(user_input: str):
+def create_prompt(user_input: str, agent_scratchpad: str = ""):
+    print(f"User Input: {user_input}")
     return ChatPromptTemplate.from_messages(
         [
             (
                 CHATBOT_ROLE.assistant.name,
-                "질문이 최근 정보를 요구하는 경우 검색 결과를 우선 제공해야 합니다.",
+                """
+                질문이 최근 정보를 요구하는 경우 검색 결과를 우선 제공해야해.
+                일상 대화는 외부 데이터 정보를 제공하지 말아줘.
+                """,
             ),
             (CHATBOT_ROLE.user.name, user_input),
+            (CHATBOT_ROLE.assistant.name, agent_scratchpad),
         ]
     )
 
@@ -25,8 +30,8 @@ def messages_prompt():
         [
             SystemMessage(
                 content="""
-    검색하는 url 링크 별로 내용을 요약해서 알려줘
-    """
+                검색하는 url 링크 별로 내용을 요약해서 알려줘
+                """
             ),
             MessagesPlaceholder(variable_name="messages"),
         ]
